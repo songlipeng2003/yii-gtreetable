5e6842e3bef07f656b81fe7ac46fe54b4cdb1cd3
Thinking Song <123wuming_123@163.com>
2014-12-08 15:46:29 +0800

    add zh_cn message

---
+++ messages/zh_cn/gtreetable.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * @author Thinking Song
+ * @copyright Copyright &copy; 2014 Thinking Song
+ * @version 2.0.0-alpha
+ * @package yii-gtreetable
+ */
+
+return array(
+    'Position indicated by parent ID is not exists!' => '父ID指示的位置不存在!',
+    'Adding operation `{name}` failed!' => '添加 `{name}` 失败!',
+    'Position is not exists!' => '位置不存在!',
+    'Main element can`t be deleted!' => '主元素不能被删除!',
+    'Deleting operation `{name}` failed!' => '删除 `{name}` 失败!',
+    'Updating operation `{name}` failed!' => '更新 `{name}` 失败!',
+    'Name' => '名称',
+);
